0595e7d36ef5acc005d23bbf54521aa59400c9e7
robertpales <pales.robo@gmail.com>
2025-10-07 19:31:08 +0200

    feat: add make_header function

---
+++ common/common.php
@@ -0,0 +1,22 @@
+<?php 
+    function make_header($title, $file) { ?>
+        <!DOCTYPE html>
+        <html lang="en">    
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <link rel="stylesheet" href="../assets/css/main.css">
+            <link rel="stylesheet" href="../assets/css/<?php echo $file?>.css">
+            <title><?php echo $title;?></title>
+        </head>
+        <body>
+    <?php 
+    }
+
+    /* TODO
+    function make_footer() {
+
+    }*/
+?>
