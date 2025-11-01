a114c7912b96a2df72db101b0398e79123f08b0b
robertpales <pales.robo@gmail.com>
2025-11-01 20:12:42 +0100

    fix conflicts

--- pages/course_detail.php
+++ pages/course_detail.php
@@ -65,27 +65,19 @@
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-                <?php if (PermissionService::isUserLoggedIn() && !$service->isUserAllreadyRegisteredToCourse($id, $_SESSION['user_id'])) { ?>
-                <div class="container text-center">
-                    <a href="actions/course_register_action.php?id=<?php echo urlencode($id); ?>" class="btn btn-primary">Zapsat se do kurzu</a>
-                </div>
-            </div>
-            <?php } ?>
-=======
-                 <?php if (PermissionService::isUserLoggedIn() && PermissionService::isUserStudent()) { 
-                $studentId = $_SESSION['user_id']; 
-                $isStudentInCourse = $myCoursesService->isStudentInCourse($studentId, $id);
+                <?php if (PermissionService::isUserLoggedIn() && PermissionService::isUserStudent()) { 
+                    $studentId = $_SESSION['user_id']; 
+                    $isStudentInCourse = $myCoursesService->isStudentInCourse($studentId, $id);
 
-                if (!$isStudentInCourse) { ?>
-                    <div class="container text-center my-5">
-                        <a href="actions/course_register_action.php?id=<?= urlencode($id); ?>" class="btn btn-primary">
-                            Zapsat se do kurzu
-                        </a>
-                    </div>
-                <?php } 
-            } ?>
->>>>>>> a370625 (feat: add admin rooms managment; add action to add, edit and delete room; update styles; update registration redirect for admin)
+                    if (!$isStudentInCourse) { ?>
+                        <div class="container text-center my-5">
+                            <a href="actions/course_register_action.php?id=<?= urlencode($id); ?>" class="btn btn-primary">
+                                Zapsat se do kurzu
+                            </a>
+                        </div>
+                    <?php } 
+                } ?>
+            </div>    
         </main>
     </div>
 
